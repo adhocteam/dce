157bc53e98868f1a77d2f4791162d17b287c8161
ahinh43 <alec4555@gmail.com>
2022-02-09 08:43:02 -0600

    Remove terraform version pinning

--- modules/main.tf
+++ modules/main.tf
@@ -1,10 +1,5 @@
 terraform {
-  required_version = "~>0.12.0"
-}
-
-provider "aws" {
-  region  = var.aws_region
-  version = "2.65.0"
+  required_version = ">=0.12.0"
 }
 
 # Current AWS Account User
